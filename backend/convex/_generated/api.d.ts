e3ff6fb03a33ba38736e30c94ed1947335b7f6f8
Abdul Rahim Mirani <abdulrahimmirani@gmail.com>
2025-09-22 22:05:45 -0400

    Added convex dep to frontend

--- backend/convex/_generated/api.d.ts
+++ backend/convex/_generated/api.d.ts
@@ -13,6 +13,10 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
+import type * as ai from "../ai.js";
+import type * as decks from "../decks.js";
+import type * as messages from "../messages.js";
+import type * as slides from "../slides.js";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -22,7 +26,12 @@ import type {
  * const myFunctionReference = api.myModule.myFunction;
  * ```
  */
-declare const fullApi: ApiFromModules<{}>;
+declare const fullApi: ApiFromModules<{
+  ai: typeof ai;
+  decks: typeof decks;
+  messages: typeof messages;
+  slides: typeof slides;
+}>;
 export declare const api: FilterApi<
   typeof fullApi,
   FunctionReference<any, "public">
