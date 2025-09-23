e65741c25ac12281902a9616b23f75efd49cdbeb
Abdul Rahim Mirani <abdulrahimmirani@gmail.com>
2025-09-22 22:13:40 -0400

    Wrapped convex in main

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { ConvexProvider, ConvexReactClient } from 'convex/react'
+
+// Initialize Convex client with your deployment URL
+const convex = new ConvexReactClient("https://fastidious-mosquito-435.convex.cloud")
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ConvexProvider client={convex}>
+      <App />
+    </ConvexProvider>
   </StrictMode>,
 )
