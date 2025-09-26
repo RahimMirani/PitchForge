509d1bd19fa714808d08de2aae9ded97012ea1da
Abdul Rahim Mirani <abdulrahimmirani@gmail.com>
2025-09-26 19:16:03 -0400

    Added auth fuctions

--- frontend/src/lib/auth-client.ts
+++ frontend/src/lib/auth-client.ts
@@ -8,3 +8,11 @@ export const authClient = createAuthClient({
   baseURL: import.meta.env.VITE_CONVEX_SITE_URL,
   plugins: [convexClient(), crossDomainClient()],
 });
+
+export const signUpUser = (name: string, email: string, password: string) =>
+  authClient.signUp({ name, email, password });
+
+export const signInUser = (email: string, password: string) =>
+  authClient.signIn({ email, password });
+
+export const signOutUser = () => authClient.signOut();
